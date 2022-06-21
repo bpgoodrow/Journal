836f85dac78673ae96ae7f9b8e421ab936a9fc54
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-21 16:16:48 -0700

    WIP all
    Co-authored-by: alex-mcknight11 <alex.mcknight11@gmail.com>

---
+++ src/journal.js
@@ -0,0 +1,32 @@
+// business logic
+
+export default function Entry(title, entry){
+  this.title = title;
+  this.entry = entry;
+};
+
+// Entry.prototype.countWords = function() {
+//   for (let i = 0; i < 10; i++) {
+//   }
+// };
+
+Entry.prototype.Vowel = function() {
+  if (input === 'a' || input === 'e' || input === 'i' || input === 'o' || input === 'u' || input === 'y') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// const wordCount() {
+//   let wordCount = " "
+//   if wordCount = 
+// }
+
+function wordCount {
+  return trim()
+}
+
+
+const str = 'I am a human'
+console.log(str.includes('a'))
